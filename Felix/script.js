c7ec94c546c9506f869f5195e81785e3c9f48a76
Blondschopf <38728132+Blondschopf@users.noreply.github.com>
2022-06-05 17:29:28 -0500

    quick update of js code to paste in p

--- Felix/script.js
+++ Felix/script.js
@@ -1,7 +1,16 @@
-let var1 = 5;
+/*let var1 = 5;
 
 if (var1 === 5) {
   console.log("Var1 es mayor que 5");
 } else {
   console.log("Var1 es menor que 5");
+}
+*/
+
+// paste text
+let input = document.getElementById("text-input");
+let output = document.getElementById("text-output");
+
+function pasteText() {
+  output.innerHTML = input.value;
 }
